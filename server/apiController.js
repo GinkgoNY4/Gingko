4f268edff8f79547968c540c666feb86bcaa41be
Zach Franz <zachfranz@Zachs-MacBook-Pro.local>
2018-06-01 16:39:51 -0400

    committing prior to pulling

---
+++ server/apiController.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+const request = require('request');
+const apiController = {};
+
+let apiLink = 'http://pokeapi.co/api/v2/pokemon/55/';
+// let apiLink = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+// let apiLink = 'https://swapi.co/api/films/1/';
+
+apiController.makeSingleReq = (req, expRes, next) => {
+	request(apiLink, { json: true }, (err, apiRes, body) => {
+	  if (err) { return console.log(err); }
+	  // console.log(apiRes);
+	  console.log(body);
+	  expRes.send({name: body.name});
+	});
+};
+
+
+module.exports = apiController;
