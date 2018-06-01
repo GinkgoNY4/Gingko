06d2f6ab91ce57cb129a3a6d10bcdb3f7009d924
Yan Yan Huang <yanyan.huang15@gmail.com>
2018-06-01 16:14:39 -0400

    fixed user routes and verification

--- server/server.js
+++ server/server.js
@@ -11,28 +11,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + './../dist'));
 
+app.post('/signup', userController.createUser);
 
-app.get('/route42', (req, res) => {
-	console.log('In route42');
-	// res.sendFile(__dirname + './../dist/index.html'); //path.resolve('temp/index.html')
-	res.sendFile(path.resolve('dist/index.html'));
-});
-
-app.get('/', (req, res) => {
-	res.send('Homepage');
-});
-
-app.get('/signup', userController.createUser);
-
-app.get('/login', userController.getAllUsers, userController.verifyUser);
-
-app.post('/signup', (req, res) => {
-	res.send(req.body);
-});
-
-app.post('/login, (req, res) => {
-	res.send(req.body);
-});
+app.post('/login', userController.verifyUser);
 
 app.listen(3000, (err, res) => {
 	if (err) return err;
--- server/userController.js
+++ server/userController.js
@@ -3,27 +3,12 @@ const User = require('../db/mongodb-orm');
 // middleware for logins && signups
 
 const userController = {};
-
-userController.getAllUsers = (req, res) => {
-	User.find({}, (err, users) => {
-		let allUsers = {};
-		
-		allUsers[username] = req.body.username;
-		allUsers[password] = req.body.password;
-		allUsers[interests] = req.body.interests;
-
-		res.send(allUsers);
-	});
-};
-
 userController.createUser = (req, res) => {
 	// console.log('req body is : ', req.body);
-	res.send(req.body);
 
 	User.create({
 		username: req.body.username,
-		password: req.body.password,
-		interests: req.body.interests
+		password: req.body.password
 	}, (err, user) => {
 		if (err) res.send('error is : ', err);
 		else res.status(200).send(user);
@@ -34,12 +19,11 @@ userController.createUser = (req, res) => {
 userController.verifyUser = (req, res, next) => {
 
 	User.findOne(req.body, (err, userInfo) => {
-		if (req.body.username === userInfo.username) {
-			res.send(userInfo);
-		} else {
+		if (userInfo == null) {
 			res.send('user does not exist, please create an account', err);
+		} else {
+			res.send(userInfo);
 		}
-
 	});
 
 };
